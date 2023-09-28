eee1b7ad745e5338ee9d495099fc1ab88158c4c7
Britt <britt@Britt-Houtmeyers.virtualbox.org>
2023-09-28 19:33:35 +0200

    Add basic CSS to setup file

---
+++ script.js
@@ -0,0 +1,18 @@
+//List of Variables, Lets, and Constants
+
+let gridWidth = 16;
+
+// List of Event Listeners
+
+
+
+// General functions
+
+
+// Normal Mode functions
+
+
+// Colorful Mode functions
+
+
+// Transparency Mode functions
